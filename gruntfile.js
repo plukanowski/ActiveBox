fbed6f6d291ad3c29e53dba83c19d2d335d14919
plukanowski <p.lukanowski@wp.pl>
2017-09-27 09:05:13 +0200

    first commit

--- gruntfile.js
+++ gruntfile.js
@@ -3,28 +3,29 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
   	sass: {
-      options: {
-        sourceMap: true
+    	options: {
+        	sourceMap: true
       },
-      dist: {
-        files: {
-          'css/main.css': 'sass/main.scss'
+      	dist: {
+        	files: {
+          		'css/main.css': 'sass/main.scss'
         }
       }
     },
 	watch: {
-    scripts: {
-        files: ['sass/*.scss'],
+    	scripts: {
+			files: ['sass/*.scss'],
         tasks: ['sass'],
         options: {
             spawn: false,
         },
-    } 
-}
+    	} 
+	},
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');	
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  	
   // Default task(s).
-  grunt.registerTask('default', ["sass"]);
+  grunt.registerTask('default', ['sass']);
 };
---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+	$('.fa-bars').on('click', function() {
+    $('.nav__menu').slideToggle();
+	});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 0){  
+    	$('nav').addClass("header__nav--sticky");
+	}
+		else{
+    	$('nav').removeClass("header__nav--sticky");
+  	}
+	});
+
+	$('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 90
+            }, 1000);
+        }
+    });
+});
