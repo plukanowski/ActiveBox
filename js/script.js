5756297b2a822540081f2b7e1c55950516d0a4ea
plukanowski <p.lukanowski@wp.pl>
2017-09-27 09:12:07 +0200

    add indents

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 $(function() {
 	$('.fa-bars').on('click', function() {
-    $('.nav__menu').slideToggle();
+		$('.nav__menu').slideToggle();
 	});
 
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 0){  
-    	$('nav').addClass("header__nav--sticky");
-	}
-		else{
-    	$('nav').removeClass("header__nav--sticky");
-  	}
+			$('nav').addClass("header__nav--sticky");
+		} else {
+			$('nav').removeClass("header__nav--sticky");
+		}
 	});
 
 	$('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top - 90
-            }, 1000);
-        }
-    });
+		var target = $(this.getAttribute('href'));
+		if(target.length) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top - 90}, 1000);
+		}
+	});
 });
